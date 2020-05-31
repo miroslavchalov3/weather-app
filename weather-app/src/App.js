819e07f7e66cd6ddc5566669ca4eca61c19363f0
miroslav chalov <m.chalov@abas-bg.com>
2020-05-31 14:34:40 +0300

    Adding basic route

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from '../src/components/homepage/HomePage'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/">
+                <HomePage></HomePage>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+    );
 }
+
+function Home() {
+    return (
+        <HomePage></HomePage>
+    );
+  }
 
 export default App;
